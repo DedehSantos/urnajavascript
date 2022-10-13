b578d8d78f79bdb6a89cdde1d3ed16452d7d631e
dedeh <dedehfrontend@gmail.com>
2022-10-13 00:46:57 -0300

    testando

---
+++ etapas.js
@@ -0,0 +1,58 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numero: 5,
+        candidatos: [
+            {
+              numero: 38111,
+              nome: 'FULANO DE TAL',
+              partido: 'ABC',
+              foto: [
+                {URL: '38111.jpg', legenda: 'Vereador', small: false}
+              ]
+            },
+            {
+                numero: 77222,
+                nome: 'beltrano da silva',
+                partido: 'DEFG',
+                foto:[
+                    {urll: '77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    
+     {
+        titulo: 'PREFEITO',
+        numero: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'CIC',
+                foto:[
+                    {url: '99.jpg', legenda: 'Prefeito'},
+                    {url: '99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            
+            },
+
+            {
+                numero: 84,
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'ZUL',
+                foto:[
+                    {url: '84.jpg', legenda: 'Prefeito'},
+                    {url: '84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            
+            },
+        ]
+     }
+
+
+
+
+];
